708a0d160684acb16b12139d15483148f2aa800d
thisal <thisalfernando39@gmail.com>
2026-03-05 16:58:51 +0530

    Modified frontend for new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,57 @@
 import './App.css';
 import React from "react";
-import CADIschemia from "./pages/CADIschemia";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import UploadECG from "./pages/UploadCADecg";
+import ECGResult from "./pages/CADEcgResult";
+
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <CADIschemia />
-    </div>
+    <>
+      <Toaster position="bottom-left" />
+      <GoogleOAuthProvider clientId="498190765675-gcl6l325mvsqp2ge1ur311lg822lt0lh.apps.googleusercontent.com">
+      <Router>
+        <Routes>
+
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+          <Route
+            path="/upload-cad-ecg"
+            element={
+              <ProtectedRoute>
+                <UploadECG />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/cad-ecg-result"
+            element={
+              <ProtectedRoute>
+                <ECGResult />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="*" element={<Navigate to="/login" />} />
+
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
+    </>
   );
 }
 
