8bf7e5ccc7c79c1ece347e36f764763564863449
Vihara Ching <liquinzeng@gmail.com>
2026-04-16 11:52:43 +0530

    port change

--- Arr_ml_service/main.py
+++ Arr_ml_service/main.py
@@ -260,4 +260,4 @@ async def general_exception_handler(request, exc):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True, log_level="info")
